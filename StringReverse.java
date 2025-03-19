a796ab423acd990db9e6381d5658bf914bce9962
Matt Callaghan <mcallaghan@naperville203.org>
2025-03-19 15:07:39 -0500

    wrote the string reverse method

---
+++ StringReverse.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class StringReverse here.
+ *
+ * @author mrcallaghan
+ * @version 19mar2025
+ */
+public class StringReverse
+{
+    public static String reverse(String str)
+    {
+        // must have a terminating case or base case (i.e., the "empty loaf")
+        if(str.equals(""))
+        {
+            return str;
+        }
+        
+        // take a small step towards the solution  (i.e., one slice of bread)
+        String firstChar = str.substring(0,1);
+        String restOfString = str.substring(1);
+        
+        // recurse - call the method with a simpler solution
+        String restOfStringReversed = reverse(restOfString);
+        String strReversed = restOfStringReversed + firstChar;
+        
+        return strReversed;
+        
+    
+    }
+}
