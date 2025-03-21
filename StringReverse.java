c2e5b2719040718e3b12aa4f983dd2a7e9b5b76b
Matt Callaghan <mcallaghan@naperville203.org>
2025-03-21 07:31:41 -0500

    Added iterative example

--- StringReverse.java
+++ StringReverse.java
@@ -25,4 +25,16 @@ public class StringReverse
         String strReversed = restOfStringReversed + firstChar;
         return strReversed;
     }
+    
+    public static String reverseIter(String str)
+    {
+        String strReversed = "";
+        
+        for(int i = 0; i < str.length(); i++)
+        {
+            strReversed = str.substring(i, i + 1) + strReversed;
+        }
+        
+        return strReversed;
+    }
 }
